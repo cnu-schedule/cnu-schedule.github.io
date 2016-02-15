23756fb533dbfb8f121f50524829ce04ab14a6a0
Allen Conlon <allen.conlon.12@cnu.edu>
2016-02-15 18:59:30 -0500

    Got the data to load from csv file, needs to optimize and for certain classes to be loaded.

--- js/index.js
+++ js/index.js
@@ -2,32 +2,38 @@ new Vue({
   el: '#classapp',
 
   data: {
-    classes: [{
-      crn :"1234",
-      cour:"ACCT 201",
-      sect:"1",
-      titl:"Prin of Account I: Financial",
-      hour:"3",
-      area:"",
-      type:"Lec",
-      days:"MWF",
-      time:"0800-0850",
-      loct:"LUTR 264",
-      ints:"Lingenfelter, G "
-    }]
+    classes: []
   },
 
   methods: {
     genClass: function(e) {
-      console.log("Hello World");
-
+      var temp, rowData;
+      var list = this.classes;
       Papa.parse("csv/201610.csv", {
         download: true,
-        setp: function(row) {
-          console.log(row.data);
-        },
         complete: function(results) {
-          console.log("DONE");
+          for (var row = 1; row < results.data.length; row++) {
+            rowData = results.data[row];
+            // console.log(rowData);
+            temp = {
+              crn :rowData[0],
+              cour:rowData[1],
+              sect:rowData[2],
+              titl:rowData[3],
+              hour:rowData[4],
+              area:rowData[5],
+              type:rowData[6],
+              days:rowData[7],
+              time:rowData[8],
+              loct:rowData[9],
+              ints:rowData[10]
+            }
+            list.push(temp);
+
+            setTimeout(function() {
+              componentHandler.upgradeDom('MaterialCheckbox');
+            }, 0);
+          }
         }
     });
     }
