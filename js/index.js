31831f6f43d73d2c1745f7a159306779244d9840
Allen Conlon <allen.conlon.12@cnu.edu>
2016-02-15 17:26:55 -0500

    Working on auto generating table

---
+++ js/index.js
@@ -0,0 +1,33 @@
+new Vue({
+  el: '#classapp',
+
+  data: {
+    classes: [{
+      crn :"1234",
+      cour:"ACCT 201",
+      sect:"1",
+      titl:"Prin of Account I: Financial",
+      hour:"3",
+      area:"",
+      type:"Lec",
+      days:"MWF",
+      time:"0800-0850",
+      loct:"LUTR 264",
+      ints:"Lingenfelter, G "
+    }]
+  },
+
+  methods: {
+    genClass: function(e) {
+      console.log("Hello World");
+
+      Papa.parse("csv/201610.csv", {
+        download: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            console.log(results.data);
+        }
+    });
+    }
+  }
+})
